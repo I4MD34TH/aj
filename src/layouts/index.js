efd1fbd4940f9adea13672768fe033de9d8029e4
Muhtasham <raja.15.isb@gmail.com>
2017-10-16 18:19:12 +0500

    first commit

---
+++ src/layouts/index.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem, Grid } from 'react-bootstrap'
+import Scrollchor from 'react-scrollchor';
+
+import './index.css'
+
+const Header = () => (
+    <Navbar collapseOnSelect className={'header'} style={{'background':'transparent', 'border':'none', 'position':'absolute', 'z-index':'100', }}>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <img src={'../images/logo.png'} />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Nav pullRight className={'social-nav'}>
+        <NavItem eventKey={2} href={"#"}><img src={'../images/fb-logo.png'} /></NavItem>
+        <NavItem eventKey={2} href={"#"}><img src={'../images/gmail-logo.png'} /></NavItem>
+        <NavItem eventKey={2} href={"#"}><img src={'../images/twitter-logo.png'} /></NavItem>
+        <NavItem eventKey={2} href={"#"}><img src={'../images/instagram-icon.png'} /></NavItem>
+      </Nav>
+      <Navbar.Collapse>
+        <Nav className={'main-nav'} pullRight style={{'margin-top':'5px', 'color': '#fff',}}>
+          <NavItem eventKey={1} href={"#"}>About</NavItem>
+          <NavItem eventKey={2} href={"#"}>Mission</NavItem>
+          <NavItem eventKey={2} href={"#"}>Get Connected</NavItem>
+          <NavItem eventKey={2} href={"#"}>Follow me</NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+)
+
+const Footer = () => (
+  <section className={'footer'} style={{'background-color':'#343434', }}>
+      <div className={'text-center news-letter'} style={{'color':'white', }}>
+        <h1><strong>GET UPDATE VIA NEWSLETTER</strong></h1>
+        <h3>Sign up to be the first to hear</h3>
+        <div className={'subscription'}>
+          <input type={'email'} className={'subscription-input'} placeholder={'Enter your Email'} />
+          <button className={'subscription-btn'}><strong>SUBCRIBE</strong></button>
+        </div>
+      </div>
+      <div className={'text-center news-letter-small'} style={{'color':'white', }}>
+        <h1><strong>GET UPDATE VIA NEWSLETTER</strong></h1>
+        <h3>Sign up to be the first to hear</h3>
+        <div className={'subscription'}>
+          <input type={'email'} className={'subscription-input'} placeholder={'Enter your Email'} />
+          <button className={'subscription-btn'}><strong>SUBCRIBE</strong></button>
+        </div>
+      </div>
+      <Grid className={'copy-right'}>
+          <div className={'copy-right-logo'}>
+            <img src={'images/footer-logo.png'} />
+            <footer style={{'color':'#c4c4c4', }}>Copyright &copy; Agency21Pvt Ltd. All rights reserved.</footer>
+          </div>
+          <div className={'social'}>
+              <a href={'https://www.facebook.com/arslan.javed/'} target={'_blank'}><img src={'../images/fb-icon.png'} style={{'height':'22px', 'padding-right':'15px', 'padding-bottom':'2px', }} /></a>
+              <a href={'https://twitter.com/Arslanjvd'} target={'_blank'}><img src={'../images/twitter-icon.png'} style={{'height':'20px', 'padding-right':'15px',}} /></a>
+              <a href={'https://www.instagram.com/arslanjaved/'} target={'_blank'}><img src={'../images/instagram.png'} style={{'height':'20px',}} /></a>
+          </div>
+      </Grid>
+  </section>
+)
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet title="Arslan Javed" meta={[{ name: 'description', content: 'Sample' }, { name: 'keywords', content: 'sample, something' }, ]}>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+    </Helmet>
+    <Header />
+    <div>
+      {children()}
+    </div>
+    <Footer />
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
---
+++ src/pages/index.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import { Button, Grid, Col, Carousel, Row } from 'react-bootstrap'
+
+const IndexPage = () => (
+  <div className={'main'}>
+    <section className={'intro'} style={{'background-image':'url(../images/background.png)', 'background-size':'cover', 'background-position':'bottom', }}>
+      <div className={'intro-content container-fluid'}>
+        <img className={'arslan-img'} src={'../images/arslan.png'} />
+        <div className={'intro-frame'}>
+          <h1 className={'text-right'}>Pakistan's Leading <br /> Real Estate <br /> Investment <br /> Advisor</h1>
+          <a className={'text-right'}><strong>L</strong>earn-<strong>M</strong>ore</a>
+          <img src={'../images/frame.png'} />
+        </div>
+        {/*<Link to="/page-2/">Go to page 2</Link>*/}
+      </div>
+    </section>
+    <section className={'about'}>
+      <Grid id={'about-1'} className={'about-1'}>
+        <div>
+          <h1 className={'about-heading'}><strong>A LITTLE ABOUT ME</strong></h1>
+          <p className={'about-para'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si alia sentit, inquam, alia loquitur, numquam intellegam quid sentiat; Virtutis, magnitudinis animi, patientiae, fortitudinis fomentis dolor mitigari solet. Id enim volumus, id contendimus, ut officii fructus sit ipsum officium.</p>
+          <p className={'about-para'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si alia sentit, inquam, alia loquitur, numquam intellegam quid sentiat; Virtutis, magnitudinis animi, patientiae, fortitudinis fomentis dolor mitigari solet. Id enim volumus, id contendimus, ut officii fructus sit ipsum officium.</p>
+          <p className={'about-para'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si alia sentit, inquam, alia loquitur, numquam intellegam quid sentiat; Virtutis, magnitudinis animi, patientiae, fortitudinis fomentis dolor mitigari solet. Id enim volumus, id contendimus, ut officii fructus sit ipsum officium.</p>
+        </div>
+        <img src={'../images/thumb.png'} />
+      </Grid>
+      <div style={{'width':'100%', }}>
+        <img className={'about-1-img'} src={'../images/thumb.png'} />
+      </div>
+      <div id={'about-2'} className={'about-2'}>
+        <Grid className={'child'}>
+          <Row className={'text-center'}>
+            <h1 className={'about-heading'}><strong>MY MISSION</strong></h1>
+            <p className={'about-para'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si alia sentit, inquam, alia loquitur, numquam intellegam quid sentiat; Virtutis, magnitudinis animi, patientiae, fortitudinis fomentis dolor mitigari solet. Id enim volumus, id contendimus, ut officii fructus sit ipsum officium.</p>
+          </Row>
+          <Row>
+            <Col xs={6} md={4}>
+              <img className={'img-responsive'} src={'../images/thumb-2.jpg'} />
+              <h4><strong>OVERSEAS PAKISTANIS RETURN</strong></h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si alia sentit, inquam, alia loquitur, numquam intellegam quid sentiat; Virtutis, magnitudinis animi, patientiae, fortitudinis fomentis dolor mitigari solet. Id enim volumus, id contendimus, ut officii fructus sit ipsum officium.</p>
+            </Col>
+            <Col xs={6} md={4}>
+              <img className={'img-responsive'} src={'../images/thumb-3.jpg'} />
+              <h4><strong>GAMECHANGER IN REALESTATE</strong></h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si alia sentit, inquam, alia loquitur, numquam intellegam quid sentiat; Virtutis, magnitudinis animi, patientiae, fortitudinis fomentis dolor mitigari solet. Id enim volumus, id contendimus, ut officii fructus sit ipsum officium.</p>
+            </Col>
+            <Col xs={6} md={4}>
+              <img className={'img-responsive'} src={'../images/thumb-4.jpg'} />
+              <h4><strong>AMAZON MALL</strong></h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si alia sentit, inquam, alia loquitur, numquam intellegam quid sentiat; Virtutis, magnitudinis animi, patientiae, fortitudinis fomentis dolor mitigari solet. Id enim volumus, id contendimus, ut officii fructus sit ipsum officium.</p>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    </section>
+    <section id={'contact'} className={'contact'}>
+      <Grid>
+        <div className={'contact-heading'}>
+          <h2 className={'ch-1'} style={{'color':'white', }}><strong>Looking for a First-Class<br /> Real Estate <span style={{'color':'#EF5350', }}>Investment Advisor</span></strong></h2>
+          <h2 className={'ch-2'} style={{'color':'white', 'font-size':'28px', }}><strong>Looking for a<br /> First-Class<br /> Real Estate <span style={{'color':'#EF5350', }}>Investment Advisor</span></strong></h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si alia sentit, inquam, alia loquitur, numquam intellegam quid sentiat; Virtutis, magnitudinis animi, patientiae, fortitudinis fomentis dolor mitigari solet.</p>
+          <button className={'contact-btn'}><strong>Contact Now</strong></button>
+        </div>
+        <img src={'images/arslan-2.png'} />
+      </Grid>
+    </section>
+    <section id={'quotes'} className={'text-center quotes'}>
+      <Grid>
+        <img src={'../images/quote-logo.png'} style={{'width':'50px', 'margin-top':'90px', 'margin-bottom':'70px', }} />
+        <div style={{'margin-top':'20px', 'margin-bottom':'150px',}}>
+          <Carousel interval={3500}>
+            <Carousel.Item>
+              <blockquote className={"twitter-tweet"}><p lang={"en"} dir={"ltr"}><i>There is no such thing as perfect decision.Every decision you make is perfected with time. &#35;Entrepreneur</i></p></blockquote>
+            </Carousel.Item>
+            <Carousel.Item>
+              <blockquote className={"twitter-tweet"}><p lang={"en"} dir={"ltr"}><i>Have more then you show. Speak less then you know.</i></p></blockquote>
+            </Carousel.Item>
+          </Carousel>
+          <p style={{'color':'#103166', 'margin-top':'30px', }}><strong>&mdash; Arslan Javed <a href={"https://twitter.com/Arslanjvd"} target={'_blank'}>&#40;&#64;Arslanjvd&#41;</a> &mdash;</strong></p>
+        </div>
+        <script async src={"//platform.twitter.com/widgets.js"} charset={"utf-8"}></script>
+      </Grid>
+    </section>
+  </div>
+)
+
+export default IndexPage
